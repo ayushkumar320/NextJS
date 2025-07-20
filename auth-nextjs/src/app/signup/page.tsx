147f426f0da86697bed75a7e05ffc9d276a65adc
ayushkumar320 <75619981+ayushkumar320@users.noreply.github.com>
2025-07-20 11:32:31 +0530

    update the frontend signup and login page

--- auth-nextjs/src/app/signup/page.tsx
+++ auth-nextjs/src/app/signup/page.tsx
@@ -5,19 +5,30 @@ import React from "react";
 import {useRouter} from "next/navigation";
 // import {axios} from "axios";
 
-export default function LoginPage() {
+export default function SignUpPage() {
   const [user, setUser] = React.useState({
     email: "",
     password: "",
+    username: "",
   });
 
-  const onLogin = async () => {};
+  const onSignup = async () => {};
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <div className="bg-gray-700 flex flex-col items-center justify-center p-12 rounded-lg px-24 border-yellow-400 border">
-        <h1 className="text-3xl mb-6">Log In</h1>
+        <h1 className="text-3xl mb-6">Sign Up</h1>
         <hr />
-        
+        <label htmlFor="username">Username</label>
+        <input
+          className="p-2 border bg-white text-black border-gray-300 rounded-md mb-4 focus:outline-none focus:border-gray-600"
+          id="username"
+          type="text"
+          value={user.username}
+          onChange={(e) => {
+            setUser({...user, username: e.target.value});
+          }}
+          placeholder="Username"
+        />
         <label htmlFor="email">Email</label>
         <input
           className="p-2 border bg-white text-black border-gray-300 rounded-md mb-4 focus:outline-none focus:border-gray-600"
@@ -41,14 +52,14 @@ export default function LoginPage() {
           placeholder="Password"
         />
         <button className="p-2 mt-4 border border-gray-300 rounded-lg mb-4 hover:bg-gray-600 hover:-translate-y-1 cursor-pointer "
-        onClick={onLogin}
+        onClick={onSignup}
         >
-          Login Here
+          Signup Here
         </button>
-        <Link href = "/signup">
+        <Link href = "/login">
           <button className="p-2 mt-4 border border-gray-300 rounded-lg mb-4 hover:bg-gray-600 hover:-translate-y-1 cursor-pointer "
         >
-          Visit Signup Page
+          Visit Login Page
         </button>
         </Link>
       </div>
