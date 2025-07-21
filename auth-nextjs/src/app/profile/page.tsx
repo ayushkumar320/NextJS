c65d758dd3fbe96b4bffa8fa185451fcc2978ef0
ayushkumar320 <75619981+ayushkumar320@users.noreply.github.com>
2025-07-21 11:16:41 +0530

    added middleware in app

--- auth-nextjs/src/app/profile/page.tsx
+++ auth-nextjs/src/app/profile/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 import axios from "axios";
 import Link from "next/link";
+import React, {useState} from "react";
 import {toast} from "react-hot-toast";
 import {useRouter} from "next/navigation";
 
 export default function ProfilePage() {
   const router = useRouter()
+  const [data, setData] = useState("nothing") 
+  
   const logout = async () => {
     try{
       await axios.get("/api/users/logout")
@@ -17,6 +20,13 @@ export default function ProfilePage() {
     }
 
   }
+
+  const getUserDetails = async () => {
+    const res = await axios.get("/api/users/me");
+    console.log(res.data);
+    setData(res.data.data._id);
+
+  }
   
   
   return (
@@ -24,11 +34,18 @@ export default function ProfilePage() {
       <h1>Profile</h1>
       <hr />
       <p>Profile Page</p>
+      <h2 className="p-1 rounded bg-green-400">{data === "nothing" ? "Nothing" : <Link href={`/profile/${data}`}>
+      {data}</Link>}</h2>
       <hr />
       <button
       onClick={logout}
       className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer"
       >Logout</button>
+      
+      <button
+      onClick={getUserDetails}
+      className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer"
+      >Get User details</button>
 
     </div>
   )
