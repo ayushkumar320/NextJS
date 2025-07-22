9f045e4a3dd5165a47ebcd87bd9c8373c3bbccfc
ayushkumar320 <75619981+ayushkumar320@users.noreply.github.com>
2025-07-22 13:24:54 +0530

    updated all the files

--- stackoverflow-appwrite/src/models/server/dbSetup.ts
+++ stackoverflow-appwrite/src/models/server/dbSetup.ts
@@ -1,33 +1,33 @@
-import {db} from "../name";
+import { db } from "../name";
 import createAnswerCollection from "./answer.collection";
 import createCommentCollection from "./comment.collection";
 import createQuestionCollection from "./question.collection";
-import createStorage from "./storageSetup";
 import createVoteCollection from "./vote.collection";
-import {databases} from "./config";
+
+import { databases } from "./config";
 
 export default async function getOrCreateDB(){
   try {
-    await databases.get(db);
-    console.log("Database connected");
-  } catch(error){
-    try{
+    await databases.get(db)
+    console.log("Database connection")
+  } catch (error) {
+    try {
       await databases.create(db, db)
-      console.log("databases connected")
-      
-      // create collection
+      console.log("database created")
+      //create collections
       await Promise.all([
         createQuestionCollection(),
         createAnswerCollection(),
         createCommentCollection(),
         createVoteCollection(),
+
       ])
-      console.log("Connection created")
-      console.log("Database connected");
+      console.log("Collection created")
+      console.log("Database connected")
     } catch (error) {
-      console.error("Error creating database or collection:", error);
+      console.log("Error creating databases or collection", error)
     }
   }
 
-  return databases;
+  return databases
 }
